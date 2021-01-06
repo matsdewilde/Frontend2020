54efb0b50b6cd7f371d51ed94ce5844548d7600c
matsdewilde <57796376+matsdewilde@users.noreply.github.com>
2021-01-07 00:23:15 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-// JavaScript Document
+var button = document.querySelector("header section:nth-of-type(1) #hamburger");
+var menu = document.querySelector("header section:nth-of-type(1)");
+
+button.addEventListener("click", klapuit);
+
+function klapuit() {
+    menu.classList.toggle("menu")
+}
